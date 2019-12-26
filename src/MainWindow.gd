d975671ca004820e6c4ddd76466d3f9ccdacf07b
James Westman <flyingpimonster@gmail.com>
2019-12-26 16:05:49 -0600

    Add version and link in the corner

--- src/MainWindow.gd
+++ src/MainWindow.gd
@@ -1,5 +1,13 @@
-extends VBoxContainer
+extends Control
 
 func show_version(version_code: String) -> void:
 	$TabContainer/Versions.select_version(version_code)
 	$TabContainer.current_tab = 1
+
+func _ready() -> void:
+	var build = ConfigFile.new()
+	build.load("res://data/build.cfg")
+	$HBox/Version.text = "v" + build.get_value("build", "version")
+
+func _on_Version_pressed() -> void:
+	OS.shell_open("https://gitlab.com/FlyingPiMonster/godot-version-manager")
